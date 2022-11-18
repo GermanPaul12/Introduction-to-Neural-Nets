b2629d42fbad0ed4c5e344cec8b224f0a7c5e514
Dattito <github@relay.datti.to>
2022-11-18 12:29:26 +0100

    change sigmoid to softmax

--- mnist_model.py
+++ mnist_model.py
@@ -9,7 +9,7 @@ def get_compiled_model(optimizer='adam', loss='mse', metrics=['accuracy']):
     model = keras.models.Sequential([
         keras.layers.Flatten(input_shape=(28, 28)),
         keras.layers.Dense(128, activation='relu'),
-        keras.layers.Dense(10, activation='sigmoid')
+        keras.layers.Dense(10, activation='softmax')
     ])
 
     # Compile model
