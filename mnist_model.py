da420c1e8c31535e53b0b260d958a52ca7e6f355
Dattito <github@relay.datti.to>
2022-11-18 12:24:01 +0100

    added GridSearchCV

--- mnist_model.py
+++ mnist_model.py
@@ -4,19 +4,7 @@ import matplotlib.pyplot as plt
 # Returns a compiled and trained model with the given hyper parameters
 
 
-def get_trained_mnist_model(epochs=10, batch_size=32, optimizer='adam', loss='mse', metrics=['accuracy'], y_one_hot_encode=True):
-    # Load MNIST data
-    (X_train, y_train), (X_test,  y_test) = keras.datasets.mnist.load_data()
-
-    # Normalize data
-    X_train = X_train / 255.0
-    X_test = X_test / 255.0
-
-    if y_one_hot_encode:
-        # One hot encode y data
-        y_train = keras.utils.to_categorical(y_train)
-        y_test = keras.utils.to_categorical(y_test)
-
+def get_compiled_model(optimizer='adam', loss='mse', metrics=['accuracy']):
     # Create model
     model = keras.models.Sequential([
         keras.layers.Flatten(input_shape=(28, 28)),
@@ -30,6 +18,27 @@ def get_trained_mnist_model(epochs=10, batch_size=32, optimizer='adam', loss='ms
         loss=loss,
         metrics=metrics
     )
+
+    # Return *compiled* model
+    return model
+
+
+def get_trained_mnist_model(epochs=10, batch_size=32, optimizer='adam', loss='mse', metrics=['accuracy'], y_one_hot_encode=True):
+
+    # Load MNIST data
+    (X_train, y_train), (X_test,  y_test) = keras.datasets.mnist.load_data()
+
+    # Normalize data
+    X_train = X_train / 255.0
+    X_test = X_test / 255.0
+
+    if y_one_hot_encode:
+        # One hot encode y data
+        y_train = keras.utils.to_categorical(y_train)
+        y_test = keras.utils.to_categorical(y_test)
+
+    model = get_compiled_model(
+        optimizer=optimizer, loss=loss, metrics=metrics, y_one_hot_encode=True)
 
     # Train model
     model.fit(X_train, y_train, epochs=epochs,
